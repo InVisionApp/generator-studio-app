faa2c7817c9ebd790af378ebaf581701f3fbd04d
Amy Troschinetz <amytroschinetz@invisionapp.com>
2018-07-18 12:20:25 -0700

    Removes a ton of cruft and patches some holes.

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,10 +4,16 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-studio-plugin:app', () => {
+  const testAuthor = 'test-author';
+  const testDescription = 'test-description';
+  const testName = 'test-name-plugin';
+
   beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ name: 'test-plugin' });
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      author: testAuthor,
+      description: testDescription,
+      name: testName
+    });
   });
 
   it('creates files', () => {
@@ -16,18 +22,22 @@ describe('generator-studio-plugin:app', () => {
       '.eslintignore',
       '.eslintrc',
       '.gitignore',
-      '.nvmrc',
+      'LICENSE',
       'package.json',
       'README.md',
-      'scripts',
       'scripts/install-plugin.js',
-      'src',
       'src/in-editor.jsx',
       'webpack.config.js'
     ].forEach(f => {
       assert.file([f]);
     });
-    assert.fileContent('README.md', '# test-plugin');
-    assert.jsonFileContent('package.json', { name: 'test-plugin' });
+    assert.fileContent('README.md', `# ${testName}`);
+    assert.fileContent('LICENSE', `${new Date().getFullYear()}`);
+    assert.fileContent('LICENSE', testAuthor);
+    assert.jsonFileContent('package.json', {
+      author: { name: testAuthor },
+      description: testDescription,
+      name: testName
+    });
   });
 });
--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,28 +1,16 @@
-/* eslint prefer-arrow-callback: 0 */
-/* eslint func-names: 0 */
-/* eslint consistent-return: 0 */
-
 const path = require('path');
 const webpack = require('webpack');
 
-const entry = {
-  'in-editor': './src/in-editor.jsx',
-};
-
-const allowedMissingModules = [];
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   target: 'node',
-  entry,
+  entry: {
+    'in-editor': './src/in-editor.jsx',
+  },
   output: {
     path: path.resolve(__dirname, 'bundle'),
     filename: '[name].js',
-  },
-  resolve: {
-    alias: {
-      styles: path.resolve(__dirname, './styles'),
-      public: path.resolve(__dirname, './public'),
-    },
   },
   module: {
     rules: [
@@ -31,46 +19,11 @@ module.exports = {
         exclude: /node_modules\/(?!(analytics-node|supports-color|has-flag)\/).*/,
         loaders: 'babel-loader',
       },
-      {
-        test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'postcss-loader',
-        ],
-      },
-      {
-        test: /\.svg$/,
-        loader: 'simple-svg-loader',
-      },
     ],
   },
-  plugins: [
-    function () {
-      this.plugin('compilation', function (compilation, data) {
-        data.normalModuleFactory.plugin('parser', function (parser) {
-          parser.plugin('call require', function (expr) {
-            if (expr.arguments.length !== 1) {
-              return;
-            }
-
-            if (allowedMissingModules.indexOf(expr.arguments[0].value) > -1) {
-              return true;
-            }
-
-            const param = this.evaluateExpression(expr.arguments[0]);
-            if (!param.isString() && !param.isConditional()) {
-              return true;
-            }
-          });
-        });
-      });
-    },
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };
